d4ba46dec32dcd79a30460dff466cbde8326edef
FoxFate <solembum.pospolity@gmail.com>
2025-05-03 18:16:51 +0200

    Add xlsxs file creator to server

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import pathlib
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,9 +9,10 @@ def createResultFile():
     name = request.json.get("name")
     payload = request.json.get("payload")
     if name:
-        filePath = pathlib.Path.cwd() / "server" / "data" / f"{name}.json"
-        with open(filePath, "w+"):
-            filePath.write_text(payload)
+        storageDir = pathlib.Path.cwd() / "server" / "data"
+        pd.read_json(payload).to_excel(storageDir / f"{name}.xlsx")
+        with open(storageDir / f"{name}.json", "w+") as f:
+            f.write(payload)
 
         return jsonify({"status": "file created"})
     return jsonify({"error": "No message provided"}), 400
